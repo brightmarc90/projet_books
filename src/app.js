4cdb03522abecfe57ffe0402f7025f5e20de99a9
Theo78360 <theo.lc78@gmail.com>
2024-11-15 10:27:31 +0100

    Ajout du app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,24 @@
-import express from 'express';
-import bookRoutes from './routes/bookRoutes.js';
+import express, { json } from 'express';
+import { connectRedis } from './config/redisConfig.js';
+import bookRoutes from './routes/bookRoutes.js';  // Routes liées aux livres
+import swaggerUi from 'swagger-ui-express';
+import { swaggerDocs } from './config/swaggerConfig.js';
 
 const app = express();
+const port = 3000;
 
-app.use(express.json());
-app.use(bookRoutes);
+// Middleware pour traiter les données JSON
+app.use(json());
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Serveur en écoute sur le port ${PORT}`);
+// Documentation Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Routes pour les livres
+app.use('/api', bookRoutes);  // Toutes les routes liées aux livres seront maintenant sous '/api'
+
+// Lancement du serveur
+connectRedis().then(() => {
+    app.listen(port, () => {
+        console.log(`Server running at http://localhost:${port}`);
+    });
 });
